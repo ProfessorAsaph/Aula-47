b80269d97c86ee521d979e0bdad4c55bead3716c
asaph-t-Souza <asaph.net@gmail.com>
2023-06-05 19:57:16 -0300

    a

---
+++ spring-backend/src/main/java/com/t3/springbackend/entity/Task.java
@@ -0,0 +1,19 @@
+package com.t3.springbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    
+}
