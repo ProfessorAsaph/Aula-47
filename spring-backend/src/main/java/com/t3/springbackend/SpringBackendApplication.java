210717ce1c89e9f788970135315959d09a1b2a94
asaph-t-Souza <asaph.net@gmail.com>
2023-06-05 22:03:07 -0300

    47

--- spring-backend/src/main/java/com/t3/springbackend/SpringBackendApplication.java
+++ spring-backend/src/main/java/com/t3/springbackend/SpringBackendApplication.java
@@ -1,7 +1,13 @@
 package com.t3.springbackend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.boot.CommandLineRunner;
+// import com.t3.springbackend.entity.Task;
+// import com.t3.springbackend.repository.TaskRepository;
+// import com.t3.springbackend.service.TaskService;
 
 @SpringBootApplication
 public class SpringBackendApplication {
@@ -9,5 +15,17 @@ public class SpringBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
 	}
+
+	// @Bean
+	// CommandLineRunner commandLineRunner (TaskRepository taskRepository){
+	// 	return args -> {
+	// 		Task task = new Task();
+	// 		task.setName("name");
+	// 		task.setDescription("descr");
+	// 		task.setDone(false);
+	// 		taskRepository.save(task);
+
+	// 	};
+	// }
 
 }
